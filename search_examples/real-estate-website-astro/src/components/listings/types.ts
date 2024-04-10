3120e3e3c83a0e6923365fe162b3f37f7c81d848
German Molina <germolinal@gmail.com>
2024-04-10 21:12:57 +1200

    storeys breakdown + cleaner code

---
+++ search_examples/real-estate-website-astro/src/components/listings/types.ts
@@ -0,0 +1,62 @@
+
+export interface Location {
+    city: string;
+    neighbourhood: string;
+}
+export interface Construction {
+    name: string;
+    layers: Layer[];
+}
+export interface Layer {
+    thickness_m: number;
+    material: string;
+}
+export type PropertySummary = {
+    location?: Location;
+    total_area_m2: number;
+    rooms: Room[];
+    constructions: Construction[];
+    in_the_neighbourhood?: string[];
+    nstoreys?: number;
+    notes: string[];
+    locale?: string;
+    images?: string[];
+};
+export type RoomPurpose =
+    | "Bathroom"
+    | "Bedroom"
+    | "Dining Room"
+    | "Kitchen"
+    | "Living Room"
+    | "Office"
+    | "Storage"
+    | "Garage"
+    | "Hallway"
+    | "Laundry"
+    | "Other";
+
+export interface Room {
+    name: string;
+    area_m2: number;
+    storey?: number;
+    purposes?: RoomPurpose[];
+}
+
+export type Index = {
+    listing_text?: string;
+    price?: number;
+    n_bedrooms?: number;
+    n_bathrooms?: number;
+    area_sqm?: number;
+    locations: string[];
+    buy_rent: "buy" | "rent";
+    property_type?: string;
+    id: string;
+    avatar?: string;
+};
+
+export type PropertyData = {
+    title?: string;
+    index: Index;
+    summary?: PropertySummary;
+};
